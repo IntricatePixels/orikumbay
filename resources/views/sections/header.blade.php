c6f86005f86cb2d2b5b180cd5c675e65d4dd8b35
Taulant Balla <taulant.balla@gmail.com>
2024-10-18 17:45:39 +0200

    Container fluid

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar sticky-top p-0 w-100">
-    <div class="header-container my-2">
+    <div class="container-fluid my-2">
         <div class="d-flex align-items-center justify-content-end">
             <button class="navbar-toggler ms-3 p-1 collapsed border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
                 <span class="navbar-toggler-icon"></span>
