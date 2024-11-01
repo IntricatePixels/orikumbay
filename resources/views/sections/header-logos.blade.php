b49fd339f68b927a18dd44cfc27081fad9b85a04
Taulant Balla <taulant.balla@gmail.com>
2024-11-01 12:05:22 +0100

    Update links

--- resources/views/sections/header-logos.blade.php
+++ resources/views/sections/header-logos.blade.php
@@ -1,15 +1,13 @@
 @php
-    // Retrieve the logo URL from the Customizer setting
     $logoUrl = get_theme_mod('kube_logo');
 @endphp
 
 @if ($logoUrl)
-    <!-- <a href="{{ esc_url(home_url('/')) }}" rel="home"> -->
-    <a href="{{ $blog_id == '1' ? '/' : '/en/' }}" rel="home">
+    <a href="{{ $blog_id == '1' ? '/al/' : '/' }}" rel="home">
         <img src="{{ esc_url($logoUrl) }}" alt="{{ esc_attr(get_bloginfo('name')) }}">
     </a>
 
 @else
-    <h1><a href="{{ $blog_id == '1' ? '/' : '/en/' }}" rel="home">{{ bloginfo('name') }}</a></h1>
+    <h1><a href="{{ $blog_id == '1' ? '/al/' : '/' }}" rel="home">{{ bloginfo('name') }}</a></h1>
     <p>{{ bloginfo('description') }}</p>
 @endif
