872f2cc16dc6f6624038194ddffd1533e8dcdbd5
Taulant Balla <taulant.balla@gmail.com>
2024-11-04 16:24:06 +0100

    Explorer update

--- resources/views/partials/explorer/explorer.blade.php
+++ resources/views/partials/explorer/explorer.blade.php
@@ -1,16 +1,17 @@
 @if (get_row_layout() == 'explorer')
 
-<section class="block bg-casa-light pt-5" id="explorer">
+<section class="block bg-casa-light pt-5" id="explorer" style="background-color: #6B635E;
+">
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 p-0">
                 @if (get_sub_field('header'))
-                    <h2 class="pt-5 pb-3 text-center font-black">
+                    <h2 class="pt-5 pb-3 text-center font-black text-white">
                         @php the_sub_field('header') @endphp
                     </h2>
                 @endif
                 @if (get_sub_field('sub_header'))
-                    <p class="pb-5 text-center">
+                    <p class="pb-5 text-center text-white">
                         @php the_sub_field('sub_header') @endphp
                     </p>
                 @endif
