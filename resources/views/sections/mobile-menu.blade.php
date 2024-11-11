e179e7c671f4b0254792aac2c3b8ffd5979ccc67
Taulant Balla <taulant.balla@gmail.com>
2024-11-11 10:33:56 +0100

    Update menu

--- resources/views/sections/mobile-menu.blade.php
+++ resources/views/sections/mobile-menu.blade.php
@@ -1,10 +1,10 @@
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
 <div class="offcanvas-header pt-5">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Main Menu</h5>
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">EN | AL</h5>
     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
     <div class="offcanvas-body d-flex flex-column p-3">
-    EN | AL
+    
         <div class="flex-fill pt-5">
             @if (has_nav_menu('primary_navigation'))
                 {!! wp_nav_menu([
