138df58ff5da6d13af2b5d25f2b9fa231d13465a
Taulant <taulant.balla@gmail.com>
2025-09-06 09:56:43 +0200

    Product grid update

--- resources/views/partials/blocks/new-block-manual-products-grid.blade.php
+++ resources/views/partials/blocks/new-block-manual-products-grid.blade.php
@@ -1,62 +1,69 @@
-
 @if (get_row_layout() == 'manual_product_grid')
     @php
         $max_width_980 = '';
-
-        if (get_sub_field('columns')) {
-            $columns = get_sub_field('columns');
-        } else {
-            $columns = "col-md-4";
-        }
-
-        if ($columns === 'col-md-6') {
-            $max_width_980 = 'max-width-980';
-        }
-
-        $column_padding = '';
-        if ($columns === 'col-md-4') {
-            $column_padding = 'pe-5';
-        }
+        $columns = get_sub_field('columns') ?: "col-md-4";
+        if ($columns === 'col-md-6') $max_width_980 = 'max-width-980';
+        $column_padding = $columns === 'col-md-4' ? 'pe-5' : '';
     @endphp
 
     <section class="block new block-products-grid in-page-section px-lg-4 px-4 px-md-0 pb-5 mb-5 {{ $max_width_980 }} py-5" id="product-grid-{{ get_row_index() }}" data-section-name="">
         <div class="container pb-5">
-            <div class="col-12 mt-lg-5">
+            <div class="col-12 mt-lg-5 text-center">
                 <p class="hero-eyebrow">{{ the_sub_field('header_eyebrow') }}</p>
                 <h2 class="mb-3 pt-0 mt-0 font-black text-center">{{ the_sub_field('header') }}</h2>
                 <div class="text-center mb-5 py-3 max-width-600 mx-auto">{{ the_sub_field('subhead') }}</div>
             </div>
-            <div class="d-flex flex-row flex-wrap justify-content-center gap-4">
+
+            <!-- FULL-WIDTH ROW LIST -->
+            <div class="row gy-4">
                 @if (have_rows('grid'))
                     @while (have_rows('grid'))
                         @php
                             the_row();
-                            $image = get_sub_field('image');
+                            $image    = get_sub_field('image');
                             $subtitle = get_sub_field('subtitle');
-                            $subtext = get_sub_field('subtext');
-                            $link = get_sub_field('link');
+                            $subtext  = get_sub_field('subtext');
+                            $link     = get_sub_field('link');
                         @endphp
-                        <div class="col-12 col-md-6 col-lg-3 border-end-custom">
-                            <div class="text-center py-lg-5 py-3 px-lg-3">
+
+                        <!-- Each card takes the full width -->
+                        <div class="col-12">
+                            <div class="d-flex flex-column flex-md-row align-items-start gap-4 py-4 px-3 border-bottom">
+                                <!-- Left: Image -->
                                 @if ($image)
-                                    <img src="{{ esc_url($image['url']) }}" alt="{{ esc_attr($image['alt']) }}" class="product-block mb-0 size-product-block" loading="lazy" />
-                                @endif
-                                @if ($subtitle)
-                                    <h2 class="prod-grid-title font-bold mt-4 mb-3 fs-5">{!! $subtitle !!}</h2>
-                                @endif
-                                @if ($subtext)
-                                    <p class="p-0"><small>{!! $subtext !!}</small></p>
-                                @endif
-                                @if ($link)
-                                    @php
-                                        $link_url = $link['url'];
-                                        $link_title = $link['title'];
-                                        $link_target = $link['target'] ? $link['target'] : '_self';
-                                    @endphp
-                                    <a class="btn btn-link position-relative mt-2" href="@php echo esc_url( $link_url ) @endphp" target="@php echo esc_attr( $link_target ) @endphp">
-                                        @php echo esc_html( $link_title ) @endphp
-                                    </a>
-                                @endif
+                                    <div class="flex-shrink-0 w-100 w-md-auto" style="width: 260px;">
+                                        <img
+                                            src="{{ esc_url($image['url']) }}"
+                                            alt="{{ esc_attr($image['alt']) }}"
+                                            class="img-fluid d-block"
+                                            loading="lazy"
+                                        />
+                                    </div>
+                                @endif>
+
+                                <!-- Right: Text -->
+                                <div class="flex-grow-1 text-start">
+                                    @if ($subtitle)
+                                        <h3 class="mb-2 fs-5 fw-bold">{!! $subtitle !!}</h3>
+                                    @endif
+
+                                    @if ($subtext)
+                                        <p class="mb-0"><small>{!! $subtext !!}</small></p>
+                                    @endif
+
+                                    @if ($link)
+                                        @php
+                                            $link_url   = $link['url'];
+                                            $link_title = $link['title'];
+                                            $link_target = $link['target'] ? $link['target'] : '_self';
+                                        @endphp
+                                        <a class="btn btn-link mt-2 px-0"
+                                           href="@php echo esc_url($link_url) @endphp"
+                                           target="@php echo esc_attr($link_target) @endphp">
+                                           @php echo esc_html($link_title) @endphp
+                                        </a>
+                                    @endif
+                                </div>
                             </div>
                         </div>
                     @endwhile
