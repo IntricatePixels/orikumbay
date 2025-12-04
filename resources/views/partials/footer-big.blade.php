549c1041752e144e8e0979fd5da253970190ffc3
Taulant Balla <taulant.balla@gmail.com>
2025-12-04 15:24:53 +0100

    Footer updates

--- resources/views/partials/footer-big.blade.php
+++ resources/views/partials/footer-big.blade.php
@@ -1,21 +1,49 @@
-<footer class="pt-0 bg-casa-light">
-    <div class="container pt-5">
+<footer class="pt-5 pb-4" style="background-color: #e8e3de;">
+    <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <?php dynamic_sidebar('footer-1'); ?>
+            <!-- Logo & Social Icons Column -->
+            <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <div class="footer-logo mb-4">
+                    @if (has_custom_logo())
+                        {!! get_custom_logo() !!}
+                    @else
+                        <img src="{{ get_theme_mod('kube_logo') }}" alt="{{ get_bloginfo('name') }}" style="max-height: 80px;">
+                    @endif
+                </div>
+                <div class="social-icons d-flex gap-3">
+                    <a href="#" class="text-dark" title="Instagram">
+                        <i class="bi bi-instagram" style="font-size: 1.5rem;"></i>
+                    </a>
+                    <a href="#" class="text-dark" title="Facebook">
+                        <i class="bi bi-facebook" style="font-size: 1.5rem;"></i>
+                    </a>
+                </div>
             </div>
-            <div class="col-md-4">
-                <?php dynamic_sidebar('footer-2'); ?>
+
+            <!-- Explore Column -->
+            <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <h5 class="mb-3 fw-normal" style="color: #6b645e;">Explore</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="/about-us" class="text-decoration-none" style="color: #6b645e;">About Us</a></li>
+                    <li class="mb-2"><a href="/contact" class="text-decoration-none" style="color: #6b645e;">Contact</a></li>
+                    <li class="mb-2"><a href="/news" class="text-decoration-none" style="color: #6b645e;">News</a></li>
+                    <li class="mb-2"><a href="/faq" class="text-decoration-none" style="color: #6b645e;">FAQ</a></li>
+                    <li class="mb-2"><a href="/services" class="text-decoration-none" style="color: #6b645e;">Our Services</a></li>
+                </ul>
             </div>
-            <div class="col-md-4">
-                <?php dynamic_sidebar('footer-3'); ?>
+
+            <!-- Location Column -->
+            <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <h5 class="mb-3 fw-normal" style="color: #6b645e;">One Oricum Bay</h5>
+                <p style="color: #6b645e;">Rruga Pashaliman</p>
+                <p style="color: #6b645e;">Orikum, Shqiperi</p>
             </div>
-        </div>
-    </div>
-    <div class="footer-copyright px-5 mt-5">
-        <div class="container">
-            <div class="row p-0">
-                <p class="text-white">&copy; All rights reserved.</p>
+
+            <!-- Contact Column -->
+            <div class="col-lg-3 col-md-6">
+                <h5 class="mb-3 fw-normal" style="color: #6b645e;">Contact</h5>
+                <p style="color: #6b645e;">+35569601580</p>
+                <p style="color: #6b645e;">info@brunesconstruction.al</p>
             </div>
         </div>
     </div>
