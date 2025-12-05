9e8a9d6fc11624110bf8d03e07dae87da024b71f
Taulant <taulant.balla@gmail.com>
2025-12-05 15:02:12 +0100

    Update phone number

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -10,8 +10,8 @@
                 </div>
                 <div class="d-flex align-items-center gap-2">
                     <i class="bi bi-telephone" style="font-size: 0.875rem; color: white;"></i>
-                    <a href="tel:+35569601580" class="text-decoration-none" style="font-size: 0.875rem; color: white;">
-                        +35569 6015802
+                    <a href="tel:+355696015802" class="text-decoration-none" style="font-size: 0.875rem; color: white;">
+                        +355696015802
                     </a>
                 </div>
                 <div class="d-flex align-items-center gap-2">
@@ -24,7 +24,7 @@
         </div>
     </div>
 
-    <nav class="navbar p-0 w-100">
+    <nav class="navbar sticky-top p-0 w-100">
     <div class="container-fluid my-2 d-flex align-items-center">
         <div class="header-logo">
             @include('sections/header-logos')
@@ -75,8 +75,8 @@
         </div>
         <div class="d-flex align-items-center gap-2 mt-2">
             <i class="bi bi-telephone"></i>
-            <a href="tel:+35569601580" class="text-decoration-none">
-                +35569 6015802
+            <a href="tel:+355696015802" class="text-decoration-none">
+                +355696015802
             </a>
         </div>
         <div class="d-flex align-items-center gap-2 mt-2">
