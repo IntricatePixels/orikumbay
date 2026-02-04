1fa40ebaf82b3423dccf0f437690b7debce72577
Taulant <taulant.balla@gmail.com>
2026-02-04 13:36:36 +0100

    fix link to go to /al/ home

--- resources/views/sections/header-logos.blade.php
+++ resources/views/sections/header-logos.blade.php
@@ -3,10 +3,10 @@
 @endphp
 
 @if ($logoUrl)
-    <a href="{{ $blog_id == '1' ? '/al/' : '/' }}" rel="home">
+    <a href="{{ home_url('/') }}" rel="home">
         <img src="{{ esc_url($logoUrl) }}" alt="{{ esc_attr(get_bloginfo('name')) }}">
     </a>
 @else
-    <h1><a href="{{ $blog_id == '1' ? '/al/' : '/' }}" rel="home">{{ bloginfo('name') }}</a></h1>
-    <p>{{ bloginfo('description') }}</p>
+    <h1><a href="{{ home_url('/') }}" rel="home">{{ get_bloginfo('name') }}</a></h1>
+    <p>{{ get_bloginfo('description') }}</p>
 @endif
